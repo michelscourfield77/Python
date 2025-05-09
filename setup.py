0a3ba732d12e317391984db57a9954affe74cc91
michelscourfield77 <85682204+michelscourfield77@users.noreply.github.com>
2025-05-09 12:20:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
+from setuptools import setup, find_packages
+
 setup(
-    name='mon_calculateur',
-    version='0.1.0',
+    name="mon_calculateur",
+    version="0.1.0",
+    author="Michel Scourfield",
+    description="Un module pour évaluer des expressions mathématiques et analyser les erreurs",
     packages=find_packages(),
-    description='Évalue une expression mathématique et compare un résultat fourni',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='michel scourfield',
-    author_email='michel@example.com',
-    url='https://github.com/michelscourfield77/Python',
-    license='MIT',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
+
+
+
+
